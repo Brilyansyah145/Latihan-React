dd954a8c97ba92013ada6da3f4c553569c1131ea
Ade Octa <ade.octa.brilyansyah2@gmail.com>
2023-04-19 20:19:57 +0700

    Define todos state and show them in the browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Finish Progate React Course',
+      complement: false,
+    },
+    {
+      id: 2,
+      title: 'Have Lunch with Guru Domba',
+      complement: false,
+    },
+    {
+      id: 3,
+      title: 'Study React with Ninja Ken',
+      complement: false,
+    },
+  ])
+  console.log(todos)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Project TodoList</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+  return(
+    <div>
+      <h1>My TodoList</h1>
+      {todos.map((todo) => {
+        return <p key= {todo.id}>{todo.title}</p>
+      })}
     </div>
   )
 }
+
+
 
 export default App
